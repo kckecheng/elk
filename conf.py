76978faed846601dc32ac2cb5b9d0c8bf9922fef
Kecheng Bi <bikecheng@gmail.com>
2019-11-12 11:03:05 +0800

    set verstion to trigger readthedocs build

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ copyright = '2019, KC'
 author = 'KC'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.2'
 
 
 # -- General configuration ---------------------------------------------------
